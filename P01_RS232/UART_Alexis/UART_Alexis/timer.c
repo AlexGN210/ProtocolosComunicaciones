91ea5c4821c9c88e05682ed68cae95840ef67a99
AlexGN210 <60145366+AlexGN210@users.noreply.github.com>
2024-04-14 20:13:10 -0600

    Add files via upload

---
+++ P01_RS232/UART_Alexis/UART_Alexis/timer.c
@@ -0,0 +1,23 @@
+/*
+ * timer.c
+ *
+ * Created: 27/03/2024 04:00:32 p. m.
+ *  Author: ShockWave
+ */ 
+#include <util/delay.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include "timer.h"
+
+void vTimerInit(void){
+	CLKPR = 0x80;
+	CLKPR = 0x00;
+
+	TCCR0A = (1 << WGM01);
+	TCCR0B = (1 << CS02) | (1 << CS00);
+	TIMSK0 = (1 << OCIE0A);
+	OCR0A = 7;
+	
+	sei();
+	
+}
---
+++ P01_RS232/UART_Alexis/UART_Alexis/timer.h
@@ -0,0 +1,13 @@
+/*
+ * timer.h
+ *
+ * Created: 27/03/2024 04:02:35 p. m.
+ *  Author: ShockWave
+ */ 
+
+#ifndef TIMER_H_
+#define TIMER_H_
+
+void vTimerInit();
+
+#endif /* TIMER_H_ */
